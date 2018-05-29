26bf710d0dcbc3454383e0b6e91c367fab93e30f
Carlos Ríos <crios@cadetech.cl>
2018-05-29 09:43:43 -0400

    fix tsv index name

--- contactplot.py
+++ contactplot.py
@@ -126,7 +126,8 @@ def plot_contact_res_bsaasa(tablefile, asafile, output,
 
 
     ax1.set_ylabel("")
-    plt.suptitle("Buried surface area in structure %s.\n%s"%(structurename, df.index.name), fontsize=20, y=0.92)
+    iname = df.index.name.split('/')[0]
+    plt.suptitle("Buried surface area in structure %s.\n%s"%(structurename, iname), fontsize=20, y=0.92)
     fig.savefig(output, dpi=dpi)
 
 
@@ -245,7 +246,7 @@ def plot_contact_atom_bsaasa(tablefile, asafile, output,
 
     ax1.set_ylabel("")
     iname = df.index.name.split('/')[0]
-    plt.suptitle("Buried surface area in structure %s.\n%s"%(structurename, df.index.name), fontsize=20, y=0.92)
+    plt.suptitle("Buried surface area in structure %s.\n%s"%(structurename, iname), fontsize=20, y=0.92)
     fig.savefig(output, dpi=dpi)
 
 
