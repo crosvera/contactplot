27633b60c24b57188907b8d5f55387b6315212d0
Carlos Ríos <crios@cadetech.cl>
2018-06-27 18:36:04 -0400

    add autosize

--- contactplot.py
+++ contactplot.py
@@ -2,13 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """Usage:
-    contactplot.py residue <tablefile> <atmasafile> [--skip-none-contact --size <widthxheight> <outputfile>]
-    contactplot.py atom  <tablefile> <atmasafile> [--skip-none-contact --size <widthxheight> <outputfile>]
-    contactplot.py ligand-protein  <tablefile> <atmasafile> [--skip-none-contact --size <widthxheight> <outputfile>]
+    contactplot.py residue <tablefile> <atmasafile> [--skip-none-contact <outputfile>]
+    contactplot.py atom  <tablefile> <atmasafile> [--skip-none-contact <outputfile>]
+    contactplot.py ligand-protein  <tablefile> <atmasafile> [--skip-none-contact <outputfile>]
 
-Options:
-    --size <widthxheight>       Output image size in pixels.
-                                [Default: 1920x1920]
 """
 from docopt import docopt
 import matplotlib as mpl
@@ -22,6 +19,7 @@ import os
 import sys
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
+from math import ceil
 
 
 def get_res_contact_area(tablefile, skip_none_contact=False):
@@ -146,10 +144,6 @@ def get_ligand_protein_bsa_vs_asa(tablefile, atmasafile, skip_none_contact=True)
             aa = a.split('/')
             ligand_bsa[aa[-1]] = contacts_df.loc[a].sum()
             
-        #protein_bsa = {(res.split('/')[-1], contacts_df[res].sum())\
-        #               for res in contacts_df.columns}
-        #ligand_bsa = {(atm.split('/')[-1], contacts_df.loc[atm].sum())\
-        #              for atm in contacts_df.index}
     elif pivot == 'index':
         for r in contacts_df.index:
             rr = r.split('/')
@@ -159,11 +153,6 @@ def get_ligand_protein_bsa_vs_asa(tablefile, atmasafile, skip_none_contact=True)
             aa = a.split('/')
             ligand_bsa[aa[-1]] = contacts_df[a].sum()
          
-        #protein_bsa = {(res.split('/')[-1],contacts_df.loc[res].sum())\
-        #               for res in contacts_df.index}
-        #ligand_bsa = {(atm.split('/')[-1],contacts_df[atm].sum())\
-        #              for atm in contacts_df.columns}
-    
     ligand_total_asa = {atm: ligand_bsa[atm]+atom_total_asa[atm]\
                         for atm in ligand_bsa}
     protein_total_asa = {res: protein_bsa[res]+res_total_asa[res]\
@@ -185,10 +174,61 @@ def plot_contact_ligand_protein(tablefile, atmasafile, output,
  
     columns = df.columns
     indexes = df.index
+    N = max(len(columns), len(indexes))
+    if N > 100:
+        sns.set_context("talk")
+    else:
+        sns.set_context("paper")
 
     structurename = tablefile.split(os.sep)[-1].split('.')[0]
     
+    dsize = 18
+    fontsizes = {
+        'xx-small': (3/5.) * dsize,
+        'x-small': (3/4.) * dsize,
+        'small': (8/9.) * dsize,
+        'medium': 1 * dsize,
+        'large': (6/5.) * dsize,
+        'x-large': (3/2.) * dsize,
+        'xx-large': 2 * dsize,
+    }
+
+
+    #adjust size
+    try:
+        fontsize_pt = fontsizes[plt.rcParams['ytick.labelsize']]
+        #print(1, fontsize_pt)
+    except KeyError:
+        fontsize_pt = plt.rcParams['ytick.labelsize']
+        #print(2, fontsize_pt)
+    matrix_height_pt = fontsize_pt * df.shape[0] * 1.05
+    matrix_height_in = matrix_height_pt / dpi
+
+    matrix_weight_pt = fontsize_pt * df.shape[1] * 1.05
+    matrix_weight_in = matrix_weight_pt / dpi
+
+    if pivot == 'index':
+        top_margin = 0.3
+        button_margin = 0.43 if N <= 100 else 0.3
+    elif pivot == 'column':
+        top_margin = 0.3
+        button_margin = 0.3
+
+    #top_margin = 0.3
+    #button_margin = 0.3
+    left_margin = 0.2
+    right_margin = 0.2
+    factor = 0.8 if N > 100 else 1.5
+    fig_height_in = matrix_height_in / (1 - top_margin - button_margin) * factor
+    fig_weight_in = matrix_weight_in / (1 - left_margin - right_margin) * factor
+
     inchsize = (int(size[0]/dpi), int(size[1]/dpi))
+    #print(inchsize)
+    size = max(fig_weight_in, fig_height_in)
+    inchsize = (ceil(size), ceil(size))
+    #print(inchsize)
+
+
     fig = plt.figure(figsize=inchsize)
 
 
@@ -235,11 +275,16 @@ def plot_contact_ligand_protein(tablefile, atmasafile, output,
                        np.arange(0.5, len(indexes)))
     ax1.scatter(X, Y, color='gray', s=3)
     
-    legend = fig.legend(loc='lower left')
+    #legend = fig.legend(loc='lower left')
+    loc = (size*0.17/N, size*0.21/N)
+    legend = fig.legend(loc=loc)
 
     ax1.set_ylabel("")
     iname = df.index.name.split('/')[0]
     plt.suptitle("Buried surface area in structure %s.\n%s"%(structurename, iname), size='xx-large', y=0.92)
+
+
+
     fig.savefig(output, dpi=dpi)
 
 
@@ -294,12 +339,56 @@ def plot_contact_res_bsaasa(tablefile, atmasafile, output,
     df = get_res_contact_area(tablefile, skip_none_contact)
     columns = df.columns
     indexes = df.index
+    N = max(len(columns), len(indexes))
+    if N > 100:
+        sns.set_context("talk")
+    else:
+        sns.set_context("paper")
 
     structurename = tablefile.split(os.sep)[-1].split('.')[0]
     
-    inchsize = (int(size[0]/dpi), int(size[1]/dpi))
-    fig = plt.figure(figsize=inchsize)
+    dsize = 18
+    fontsizes = {
+        'xx-small': (3/5.) * dsize,
+        'x-small': (3/4.) * dsize,
+        'small': (8/9.) * dsize,
+        'medium': 1 * dsize,
+        'large': (6/5.) * dsize,
+        'x-large': (3/2.) * dsize,
+        'xx-large': 2 * dsize,
+    }
 
+
+    #adjust size
+    try:
+        fontsize_pt = fontsizes[plt.rcParams['ytick.labelsize']]
+        #print(1, fontsize_pt)
+    except KeyError:
+        fontsize_pt = plt.rcParams['ytick.labelsize']
+        #print(2, fontsize_pt)
+    matrix_height_pt = fontsize_pt * df.shape[0] * 1.05
+    matrix_height_in = matrix_height_pt / dpi
+
+    matrix_weight_pt = fontsize_pt * df.shape[1] * 1.05
+    matrix_weight_in = matrix_weight_pt / dpi
+
+    top_margin = 0.3
+    button_margin = 0.3
+    left_margin = 0.3
+    right_margin = 0.3
+    factor = 0.8 if N > 100 else 1.6
+    fig_height_in = matrix_height_in / (1 - top_margin - button_margin) * factor
+    fig_weight_in = matrix_weight_in / (1 - left_margin - right_margin) * factor
+
+
+    inchsize = (int(size[0]/dpi), int(size[1]/dpi))
+    #print(inchsize)
+    size = max(fig_weight_in, fig_height_in)
+    inchsize = (ceil(size), ceil(size))
+    #print(inchsize)
+
+
+    fig = plt.figure(figsize=inchsize)
 
 
     #cbar formatter:
@@ -340,7 +429,9 @@ def plot_contact_res_bsaasa(tablefile, atmasafile, output,
                        np.arange(0.5, len(indexes)))
     ax1.scatter(X, Y, color='gray', s=3)
     
-    legend = fig.legend(loc='lower left')
+    #legend = fig.legend(loc='lower left')
+    loc = (size*0.23/N, size*0.23/N)
+    legend = fig.legend(loc=loc)
 
     ax1.set_ylabel("")
     iname = df.index.name.split('/')[0]
@@ -383,13 +474,54 @@ def plot_contact_atom_bsaasa(tablefile, atmasafile, output,
     df = get_atom_contact_area(tablefile, skip_none_contact)
     columns = df.columns
     indexes = df.index
-
+    N = max(len(columns), len(indexes))
+    if N > 100:
+        sns.set_context("paper")
+    else:
+        sns.set_context("talk")
 
     structurename = tablefile.split(os.sep)[-1].split('.')[0]
     
-    inchsize = (int(size[0]/dpi), int(size[1]/dpi))
-    fig = plt.figure(figsize=inchsize)
+    dsize = 18
+    fontsizes = {
+        'xx-small': (3/5.) * dsize,
+        'x-small': (3/4.) * dsize,
+        'small': (8/9.) * dsize,
+        'medium': 1 * dsize,
+        'large': (6/5.) * dsize,
+        'x-large': (3/2.) * dsize,
+        'xx-large': 2 * dsize,
+    }
 
+
+    #adjust size
+    try:
+        fontsize_pt = fontsizes[plt.rcParams['ytick.labelsize']]
+    except KeyError:
+        fontsize_pt = plt.rcParams['ytick.labelsize']
+
+    matrix_height_pt = fontsize_pt * df.shape[0] 
+    matrix_height_in = matrix_height_pt / dpi
+
+    matrix_weight_pt = fontsize_pt * df.shape[1]
+    matrix_weight_in = matrix_weight_pt / dpi
+
+    top_margin = 0.35
+    button_margin = 0.35
+    left_margin = 0.35
+    right_margin = 0.35
+    factor = 0.5 if N > 100 else 1.
+    fig_height_in = matrix_height_in / (1 - top_margin - button_margin) * factor
+    fig_weight_in = matrix_weight_in / (1 - left_margin - right_margin) * factor
+
+    inchsize = (int(size[0]/dpi), int(size[1]/dpi))
+    #print(inchsize)
+    size = ceil(max(fig_weight_in, fig_height_in))
+    inchsize = (int(size), int(size))
+    #print(inchsize)
+
+
+    fig = plt.figure(figsize=inchsize)
 
 
     #cbar formatter:
@@ -426,7 +558,8 @@ def plot_contact_atom_bsaasa(tablefile, atmasafile, output,
                        np.arange(0.5, len(indexes)))
     ax1.scatter(X, Y, color='gray', s=3)
     
-    legend = fig.legend(loc='lower left')
+    loc = (size*0.6/N, size*0.6/N)
+    legend = fig.legend(loc=loc)
 
     ax1.set_ylabel("")
     iname = df.index.name.split('/')[0]
@@ -446,13 +579,15 @@ def check_tablefile_type(tablefile):
 
 def contactplot():
     args = docopt(__doc__)
-    size = tuple(int(s) for s in args['--size'].split('x'))
     skip_none_contact = args['--skip-none-contact']
     tablefile = args['<tablefile>']
     atmasafile = args['<atmasafile>']
     output = args['<outputfile>']
     if not output:
-        output = os.path.splitext(tablefile)[0] + '.png'
+        if args['ligand-protein']:
+            output = os.path.splitext(tablefile)[0] + '_ligand_protein.png'
+        else:
+            output = os.path.splitext(tablefile)[0] + '.png'
 
     if atmasafile.split('.')[-1] != 'atmasa':
         print("ERROR: {} is not the .atmasa file.".format(atmasafile))
@@ -462,12 +597,12 @@ def contactplot():
         if check_tablefile_type(tablefile)[0] != 'by_res':
             print ("ERROR: Incorrect tablefile, you should use the *.by_res.tsv file.")
             sys.exit(-1)
-        plot_contact_res_bsaasa(tablefile, atmasafile, output, skip_none_contact, size)
+        plot_contact_res_bsaasa(tablefile, atmasafile, output, skip_none_contact)
     elif args['atom']:
         if check_tablefile_type(tablefile)[0] != 'by_atom':
             print ("ERROR: Incorrect tablefile, you should use the *.by_atom.tsv file.")
             sys.exit(-1)
-        plot_contact_atom_bsaasa(tablefile, atmasafile, output, skip_none_contact, size)
+        plot_contact_atom_bsaasa(tablefile, atmasafile, output, skip_none_contact)
     elif args['ligand-protein']:
         if check_tablefile_type(tablefile)[0] != 'by_atom':
             print ("ERROR: Incorrect tablefile, you should use the *.by_atom.tsv file.")
@@ -475,7 +610,7 @@ def contactplot():
         if check_tablefile_type(tablefile)[1] == 'matrix':
             print ("ERROR: Table file type 'matrix' still not supported in this mode.")
             sys.exit(-1)
-        plot_contact_ligand_protein(tablefile, atmasafile, output, skip_none_contact, size)
+        plot_contact_ligand_protein(tablefile, atmasafile, output, skip_none_contact)
 
 
 if __name__ == '__main__':
